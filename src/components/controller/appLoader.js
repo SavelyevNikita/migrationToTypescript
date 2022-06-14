603a0f428aae799dd0dc051485f4a45d95d960d7
SavelyevNikita <nik2111122@sibmail.com>
2022-06-14 11:25:35 +0300

    init: start migration

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '12e7ef50353e443d998bc1ff56a76034', // получите свой ключ https://newsapi.org/
         });
     }
 }
